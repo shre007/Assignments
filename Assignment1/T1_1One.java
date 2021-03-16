3e34a9cb7e29908a383708d1979eae72ca86ff80
shre007 <56560801+shre007@users.noreply.github.com>
2021-03-16 13:46:24 -0500

    Create T1_1One.java

---
+++ Assignment1/T1_1One.java
@@ -0,0 +1,50 @@
+//Question 1
+//Swap two numbers using the third variable as a result name and do the same task without usingany third variable.
+package task_1;
+//package task_1;
+import java.util.*;
+import java.util.Scanner;
+class Task1 {
+///FIRST RUN QUESTION 1A, THEN COMMENT IT AND RUN 1B
+//Question 1a
+	public static void main(String []s)
+		{
+		int a,b;
+//			//Scanner class to read value
+			Scanner sc=new Scanner(System.in);
+
+			System.out.print("Enter value of a: ");
+			a=sc.nextInt();
+			System.out.print("Enter value of a: ");
+			b=sc.nextInt();
+
+			System.out.println("Before swapping - a: "+ a +", b: " + b);
+//			////without using third variable
+			a=a+b;
+			b=a-b;
+			a=a-b;
+//			//////////////////////
+			System.out.println("After swapping  - a: "+ a +", b: " + b);
+		}
+	}
+//Question 1b
+	public static void main(String []s)
+	{
+/		int a,b;
+////		//Scanner class to read value
+		Scanner sc=new Scanner(System.in);
+///
+		System.out.print("Enter value of a: ");
+		a=sc.nextInt();
+		System.out.print("Enter value of a: ");
+		b=sc.nextInt();
+////
+		System.out.println("Before swapping - a: "+ a +", b: " + b);
+////		////without using third variable
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		//////////////////////
+		System.out.println("After swapping  - a: "+ a +", b: " + b);
+	}
+}
