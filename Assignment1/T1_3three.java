3775a0477936c86f02a5f1a5db059ff73b86749a
shre007 <56560801+shre007@users.noreply.github.com>
2021-03-16 16:06:31 -0500

    Update T1_3three.java

--- Assignment1/T1_3three.java
+++ Assignment1/T1_3three.java
@@ -1,5 +1,24 @@
 package task_1;
+import java.util.*;
+import java.util.Scanner;
 
 public class T1_3three {
+public static void main(String[] args) {
+int num1,num2,z, result;
+Scanner input =  new Scanner(System.in);
+System.out.println("Enter first number between 1 to 10");
+num1 = input.nextInt();
+System.out.println("Enter second number between 1 to 10");
+num2 = input.nextInt();
+System.out.println("Enter value of z as 30");
+z = input.nextInt();
+input.close();
+////
+result = num1 + num2 + z;
+////
+System.out.printf("%d + %d + %d = %d", num1,num2,z,result);
 
 }
+
+}
+
