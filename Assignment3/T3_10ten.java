2422699db302d7ad111ab9f78ffac6aa97cbe167
shre007 <56560801+shre007@users.noreply.github.com>
2021-03-20 02:12:02 -0500

    Add files via upload

---
+++ Assignment3/T3_10ten.java
@@ -0,0 +1,44 @@
+//Question 10
+
+//Write a Java program to display Pascal's triangle.
+//Test Data
+//Input number of rows: 5
+//Expected Output :
+//Input number of rows: 5 
+// 1 
+// 1 1 
+// 1 2 1 
+// 1 3 3 1 
+//1 4 6 4 1
+
+
+package task_3;
+import java.util.Scanner;
+public class T3_10ten {
+
+	public static void main(String[] args)
+	{
+	    int no_row;
+	    int c=1;
+	    int blk;
+	    int i;
+	    int j;
+	    System.out.print("Enter number of rows: ");
+	    Scanner in = new Scanner(System.in);
+			    no_row = in.nextInt();
+	    for(i=0;i<no_row;i++)
+	    {
+	        for(blk=1;blk<=no_row-i;blk++)
+	        System.out.print(" ");
+	        for(j=0;j<=i;j++)
+	        {
+	            if (j==0||i==0)
+	                c=1;
+	            else
+	               c=c*(i-j+1)/j;
+	            System.out.print(" "+c);
+	        }
+	        System.out.print("\n");
+	    }
+	}
+	}
