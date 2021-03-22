484888e7534944ec9e6540d849da36fd8185c1f4
shre007 <56560801+shre007@users.noreply.github.com>
2021-03-22 14:49:41 -0500

    Add files via upload

---
+++ Assignment3/T3_5five.java
@@ -0,0 +1,15 @@
+package task_3;
+import java.util.*;
+
+public class T3_5five 
+{  
+    public static void main(String[] args) {  
+        Scanner input = new Scanner(System.in);  
+        System.out.print("Enter first number: ");  
+          long num1 = input.nextLong(); 
+          System.out.print("Enter second number: ");  
+      	  long num2= input.nextLong();
+          System.out.println("The result is: "+(num1 + num2));  
+          input.close();  
+          }  
+}  
