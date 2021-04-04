8d85110994a73c5fa08ede3127229cd7ad27c9b3
shre007 <56560801+shre007@users.noreply.github.com>
2021-04-04 13:10:21 -0500

    Create T7_1one.java

---
+++ Assignment7/T7_1one.java
@@ -0,0 +1,17 @@
+//Question 1
+//Program to reverse a string in java
+
+
+package task_7;
+
+public class T7_1one {
+  
+	    public static void main(String[] args)
+	    {
+	        String input = "This is it";
+	        StringBuilder input1 = new StringBuilder();
+	        input1.append(input);
+	        input1.reverse();
+	        System.out.println(input1);
+	    }
+	}
